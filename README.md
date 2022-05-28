88d27e8a560482926ed5f3015168add8a6be0660
Frans de Waal <fmdewaal@gmail.com>
2022-05-29 00:58:10 +0200

    Basic route handling

--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 ## Notes
 
 - https://www.youtube.com/watch?v=osANZ9aigIg
+- https://nodejs.org/docs/latest-v16.x/api/http.html
--- server.js
+++ server.js
@@ -2,12 +2,30 @@ const http = require('http')
 
 let count = 0
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200 
-  res.setHeader('Content-Type', 'application/json')
-  count++
-  console.log(`Count: ${count}`);
-  res.end(`{"data":"OK","count":${count}}`)
+// https://nodejs.org/docs/latest-v16.x/api/http.html#httpcreateserveroptions-requestlistener
+const server = http.createServer((
+  request, // https://nodejs.org/docs/latest-v16.x/api/http.html#class-httpincomingmessage
+  response, // https://nodejs.org/docs/latest-v16.x/api/http.html#class-httpserverresponse
+) => {
+  const requestURL = new URL(request.url, `http://${request.headers.host}`);
+
+  if (requestURL.pathname === '/count') {
+    // console.log('requestURL', requestURL);
+
+    count++
+    console.log(`Count: ${count}`);
+
+    response.statusCode = 200
+    response.setHeader('Content-Type', 'application/json')
+
+    const data = {
+      count
+    }
+    response.end(JSON.stringify(data))
+  } else {
+    response.statusCode = 200
+    response.end()
+  }
 })
 
 server.listen(8000)
