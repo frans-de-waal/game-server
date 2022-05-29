f75d0d0262c1fdb40281ecc8a66e8d2da14a4f76
Frans de Waal <fmdewaal@gmail.com>
2022-05-29 19:24:40 +0200

    Handle errors in handlers

--- src/server.js
+++ src/server.js
@@ -3,13 +3,21 @@ import getRouteHandler from './utils/getRouteHandler.js'
 import getRoute from './utils/getRoute.js'
 
 // https://nodejs.org/docs/latest-v16.x/api/http.html#httpcreateserveroptions-requestlistener
-const server = http.createServer((
+const server = http.createServer(async (
   request, // https://nodejs.org/docs/latest-v16.x/api/http.html#class-httpincomingmessage
   response, // https://nodejs.org/docs/latest-v16.x/api/http.html#class-httpserverresponse
 ) => {
   const routeHandler = getRouteHandler(getRoute(request))
 
-  routeHandler(request, response)
+  try {
+    await routeHandler(request, response)
+  } catch(error) {
+    response.statusCode = 500
+    response.setHeader('Content-Type', 'application/json')
+    response.end(JSON.stringify({
+      error: error.message
+    }))
+  }
 })
 
 const port = process.env.PORT || 8000
